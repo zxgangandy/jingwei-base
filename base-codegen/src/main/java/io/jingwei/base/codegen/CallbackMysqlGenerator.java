3c926eda91b8d99cd19301e4df5c704248149a60
andy zhao <286505491@qq.com>
2020-11-23 17:32:18 +0800

    完善逻辑;

---
+++ base-codegen/src/main/java/io/jingwei/base/codegen/CallbackMysqlGenerator.java
@@ -0,0 +1,40 @@
+package io.jingwei.base.codegen;
+
+
+/**
+ * <p>
+ * mysql 代码生成器
+ * </p>
+ */
+public class CallbackMysqlGenerator extends BaseMysqlGenerator {
+
+    @Override
+    protected String getDbName() {
+        return "callback-center";
+    }
+
+    @Override
+    protected String getModuleName() {
+        return "callback-service";
+    }
+
+    @Override
+    protected String getPkgName() {
+        return "io.github.zxgangandy.callback";
+    }
+
+    @Override
+    protected String getTableName() {
+        return "callback_task";
+    }
+
+    @Override
+    protected boolean projectDir() {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        CallbackMysqlGenerator generator = new CallbackMysqlGenerator();
+        generator.run();
+    }
+}
