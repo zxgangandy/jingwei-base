eb77119ed5cd520cfa22a2a56875a89f60792250
andy zhao <286505491@qq.com>
2020-11-30 17:29:45 +0800

    增加时间处理方法;

--- base-utils/src/main/java/io/jingwei/base/utils/time/DateUtils.java
+++ base-utils/src/main/java/io/jingwei/base/utils/time/DateUtils.java
@@ -1,9 +1,7 @@
 package io.jingwei.base.utils.time;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class DateUtils {
@@ -22,5 +20,43 @@ public class DateUtils {
 
     public static LocalDateTime asLocalDateTime(Date date) {
         return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+
+    //获取当前时间(字符串格式)
+    public static String getCurrentDateTime(String format) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        return LocalDateTime.now().format(formatter);
+    }
+
+    //获取昨天时间格式
+    public static String getYesterdayByFormat(String format) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        return LocalDateTime.now().minusDays(1).format(formatter);
+    }
+
+    //获取从1970年1月1日到现在的秒数
+    public static Long getSecond(String dateTime, String format) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern(format);
+        LocalDateTime localDateTime = LocalDateTime.parse(dateTime, dtf);
+        Long second = localDateTime.toEpochSecond(ZoneOffset.of("+8"));
+        return second;
+    }
+
+    //LocalDateTime转String
+    public static String getLocalDateTimeToStr(LocalDateTime localDateTime, String format) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern(format);
+        return localDateTime.format(dtf);
+    }
+
+    //获取从1970年1月1日到现在的秒数转LocalDateTime
+    public static LocalDateTime getSecondToLocalDateTime(Long second) {
+        Instant instant = Instant.ofEpochSecond(second);
+        ZoneId zoneId = ZoneId.systemDefault();
+        return LocalDateTime.ofInstant(instant, zoneId);
+    }
+
+    //获取从1970年1月1日到现在的秒数转字符串格式
+    public static String getSecondToStr(long second, String format) {
+        return getLocalDateTimeToStr(getSecondToLocalDateTime(second), format);
     }
 }
