b5b45287709d4bbb85bbf7fb323b2d0aa818f182
andy zhao <286505491@qq.com>
2021-01-22 15:43:34 +0800

    增加订单冻结逻辑;

---
+++ base-codegen/src/main/java/io/jingwei/base/codegen/AccountMysqlGenerator.java
@@ -0,0 +1,40 @@
+package io.jingwei.base.codegen;
+
+
+/**
+ * <p>
+ * mysql 代码生成器
+ * </p>
+ */
+public class AccountMysqlGenerator extends BaseMysqlGenerator {
+
+    @Override
+    protected String getDbName() {
+        return "ex_account";
+    }
+
+    @Override
+    protected String getModuleName() {
+        return "exchange-account";
+    }
+
+    @Override
+    protected String getPkgName() {
+        return "com.zxgangandy.account";
+    }
+
+    @Override
+    protected String getTableName() {
+        return "spot_account_frozen";
+    }
+
+    @Override
+    protected boolean projectDir() {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        AccountMysqlGenerator generator = new AccountMysqlGenerator();
+        generator.run();
+    }
+}
