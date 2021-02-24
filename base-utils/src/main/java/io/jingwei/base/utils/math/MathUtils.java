07627a6e3990b062f33f7debdbc2ed2de98f5a30
andy zhao <286505491@qq.com>
2021-02-24 11:28:23 +0800

    update math util;

--- base-utils/src/main/java/io/jingwei/base/utils/math/MathUtils.java
+++ base-utils/src/main/java/io/jingwei/base/utils/math/MathUtils.java
@@ -1,11 +1,20 @@
 package io.jingwei.base.utils.math;
 
-
 import org.apache.commons.lang3.StringUtils;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class MathUtils {
+    /**
+     * 计算精度
+     */
+    public static Integer SCALE = 16;
+
+    /**
+     * 进位规则, 默认全部舍弃
+     */
+    public static RoundingMode MODE = RoundingMode.DOWN;
 
     public static BigDecimal toScale(int scale, String value) {
         if (StringUtils.isEmpty(value)) {
@@ -27,5 +36,25 @@ public class MathUtils {
         }
 
         return value.stripTrailingZeros().toPlainString();
+    }
+
+    public static BigDecimal div (BigDecimal a,BigDecimal b) {
+        return a.divide(b, SCALE, MODE);
+    }
+
+    public static BigDecimal mul (BigDecimal a, BigDecimal b) {
+        return a.multiply(b).setScale(SCALE,MODE);
+    }
+
+    public static BigDecimal add (BigDecimal a, BigDecimal b) {
+        return a.add(b);
+    }
+
+    public static BigDecimal sub(BigDecimal a,BigDecimal b) {
+        return a.subtract(b);
+    }
+
+    public static BigDecimal round (BigDecimal val) {
+        return val.setScale(SCALE, MODE);
     }
 }
