11400f655b973a6c0c3a1e8bb8fb14c58632091e
andy zhao <286505491@qq.com>
2021-04-04 14:44:54 +0800

    update version to 1.0.9;

--- base-idgen/src/main/java/io/jingwei/base/idgen/UidAutoConfigure.java
+++ base-idgen/src/main/java/io/jingwei/base/idgen/UidAutoConfigure.java
@@ -6,13 +6,16 @@ import io.jingwei.base.idgen.impl.DefaultUidGenerator;
 import io.jingwei.base.idgen.impl.UidProperties;
 import io.jingwei.base.idgen.worker.DisposableWorkerIdAssigner;
 import io.jingwei.base.idgen.worker.WorkerIdAssigner;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
 
 /**
  * UID 的自动配置
@@ -23,6 +26,8 @@ import org.springframework.context.annotation.Lazy;
 @Configuration
 @ConditionalOnClass({ DefaultUidGenerator.class, CachedUidGenerator.class })
 @EnableConfigurationProperties(UidProperties.class)
+@MapperScan(basePackages = {"io.jingwei.base.idgen.worker.mapper"})
+@ComponentScan(basePackages = {"io.jingwei.base.idgen.worker"})
 public class UidAutoConfigure {
 
 	@Autowired
